5a9e370cd04fe2ce124dde356984cbd02990ab3e
Federico Sordillo <federicosordillo@gmail.com>
2017-04-28 16:30:46 +0200

    initial

---
+++ main.js
@@ -0,0 +1,16 @@
+import hyperHTML from 'hyperhtml'
+
+console.log(hyperHTML)
+console.log(document.getElementById('root'))
+
+function tick (render) {
+  render`
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is ${new Date().toLocaleTimeString()}.</h2>
+    </div>
+                          `
+}
+setInterval(tick, 1000,
+              hyperHTML.bind(document.getElementById('root'))
+           )
